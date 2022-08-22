633ee3ca07f7f7cfc1031745421b9a1ff513263d
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2022-08-22 15:08:15 +0200

    hamburger wip

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -4,29 +4,32 @@
     <nav v-if="!hamburger_active">
       <ul>
         <li>
-            <router-link class="squadron" to="/">
-              <h2 class="hidden">Home</h2>
-              <div class="home">
-                <WombatLogo></WombatLogo>
-                <div class="squadron_number">VMFA-117</div>
+          <router-link class="squadron" to="/">
+            <div class="home">
+              <div class="home_img">
+                <img src="https://wombats.s3.eu-central-1.amazonaws.com/wombat_logo.svg" alt="VMFA117 logo">
+              </div>
+              <div class="home_txt">
+                <h2 class="squadron_number">VMFA-117</h2>
                 <div class="squadron_name tinos">MIGHTY<br>WOMBATS</div>
               </div>
-            </router-link>
+            </div>
+          </router-link>
         </li>
         <li>
-          <router-link to="/greenie"><h2>Greenie Board</h2></router-link>
+          <router-link to="/greenie"><h2 >Greenie Board</h2></router-link>
         </li>
         <li>
-          <router-link to="/log"><h2>Mission Log</h2></router-link>
+          <router-link to="/log"><h2 >Mission Log</h2></router-link>
         </li>
         <li>
-          <router-link to="/gallery"><h2>Galeria</h2></router-link>
+          <router-link to="/gallery"><h2 >Galeria</h2></router-link>
         </li>
         <li>
-          <router-link to="/contact"><h2>Kontakt/Rekrutacja</h2></router-link>
+          <router-link to="/contact"><h2 >Rekrutacja</h2></router-link>
         </li>
         <li>
-          <router-link to="/login"><h2>Zaloguj</h2></router-link>
+          <router-link to="/login"><h2 >Zaloguj</h2></router-link>
         </li>
       </ul>
     </nav>
@@ -38,7 +41,7 @@ import WombatLogo from "@/components/elements/WombatLogo";
 
 export default {
   name: "TheHeader",
-  components: { WombatLogo },
+  components: {WombatLogo},
   data() {
     return {
       hamburger_active: window.innerWidth < 800
@@ -53,99 +56,111 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home {
-  WombatLogo{
-    width: 200px;
-    height: 200px;
 
-  }
-  display: flex;
-  flex-direction: row;
-}
 header {
-  //width: 100vw;
   height: $header_height;
+  width: 100%;
   position: fixed;
   background-color: $overshadow;
-  display: flex;
-  //justify-content: center;
-  //align-items: center;
   z-index: 100;
 
-  //a {
-  //  text-decoration: none;
-  //  color: $wombat_white;
-  //  display: inline-block;
-  //  padding: 0.75rem 1.5rem;
-  //  border: 1px solid transparent;
-  //  font-size: 1.5rem;
-  //  font-weight: bold;
-  //
-  //  &:active,
-  //  &:hover,
-  //  &.router-link-active {
-  //    color: $wombat_yellow;
-  //    text-shadow: $wombat_yellow 2px 2px 25px;
-  //  }
-  //
-  //  &.squadron {
-  //    text-align: left;
-  //    line-height: 95%;
-  //    padding: 0;
-  //
-  //    .squadron_number {
-  //      font-size: .75rem;
-  //    }
-  //
-  //    .squadron_name {
-  //      font-size: 2.5rem;
-  //    }
-  //
-  //    &:active,
-  //    &:hover,
-  //    &.router-link-active {
-  //      color: $wombat_white;
-  //      text-shadow: none;
-  //    }
-  //  }
-  //}
-  //
-  //h1 {
-  //
-  //  margin: 0;
-  //
-  //  a {
-  //    color: $wombat_white;
-  //    margin: 0;
-  //    font-size: 2rem;
-  //
-  //    &:hover,
-  //    &:active,
-  //    &.router-link-active {
-  //      border-color: transparent;
-  //    }
-  //  }
-  //}
-
   nav {
-    width: 100vw;
-    //margin: auto;
-    display: flex;
-    flex-direction: row;
-    //justify-content: space-between;
-    //align-items: center;
-  }
+    width: inherit;
+    height: inherit;
 
-  ul {
-    list-style: none;
-    //margin: 0;
-    //padding: 0;
-    display: flex;
-    //justify-content: center;
-    align-items: center;
+    ul {
+      width: inherit;
+      height: inherit;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
 
+      li {
+        a {
+          display: block;
+          color: $wombat_white;
+          font-size: 1.5rem;
+          font-weight: bold;
+          .home {
+            .squadron_number{
+              font-size: 1rem;
+            }
+            display: flex;
+            flex-direction: row;
+            align-content: center;
+            justify-content: center;
+            .home_img {img {
+              height: $header_height;
+            }
+
+            }
+            .home_txt {
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+            }
+
+
+          }
+        }
+      }
+    }
   }
 }
 
+//a {
+//  text-decoration: none;
+//  color: $wombat_white;
+//  display: inline-block;
+//  padding: 0.75rem 1.5rem;
+//  border: 1px solid transparent;
+//  font-size: 1.5rem;
+//  font-weight: bold;
+//
+//  &:active,
+//  &:hover,
+//  &.router-link-active {
+//    color: $wombat_yellow;
+//    text-shadow: $wombat_yellow 2px 2px 25px;
+//  }
+//
+//  &.squadron {
+//    text-align: left;
+//    line-height: 95%;
+//    padding: 0;
+//
+//    .squadron_number {
+//      font-size: .75rem;
+//    }
+//
+//    .squadron_name {
+//      font-size: 2.5rem;
+//    }
+//
+//    &:active,
+//    &:hover,
+//    &.router-link-active {
+//      color: $wombat_white;
+//      text-shadow: none;
+//    }
+//  }
+//}
+//
+//h1 {
+//
+//  margin: 0;
+//
+//  a {
+//    color: $wombat_white;
+//    margin: 0;
+//    font-size: 2rem;
+//
+//    &:hover,
+//    &:active,
+//    &.router-link-active {
+//      border-color: transparent;
+//    }
+//  }
+//}
 
 </style>
