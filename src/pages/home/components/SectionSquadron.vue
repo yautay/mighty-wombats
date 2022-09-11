007774bf62021e9b91ad4d7ff540525ac88ddd59
yautay <yautay@gmail.com>
2022-09-11 20:44:02 +0200

    vmfa-177 section wip

--- src/pages/home/components/SectionSquadron.vue
+++ src/pages/home/components/SectionSquadron.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <h2>VMFA117</h2>
+    <h2 class="hidden">Squadron</h2>
+    <div class="squadron_info">
+      <div class="content_block">
+        <div class="content_element title tinos">
+          <div class="wombat_line"></div>
+          <div class="wrapper_title"><p>VMFA-117 Mighty Wombats</p></div>
+        </div>
+        <div class="content_element description">
+          <p>Profil Misji: Szeroko pojęte wspierania działań desantowych USMC. Latamy na najnowszej Open Beta.</p>
+          <br>
+          <p>Chodzi nam o dobrą zabawę w przyjemnej atmosferze. Jednocześnie, to jest hobby. Ludzie wkładają w eskadrę
+            wysiłek i sporo cennego czasu. Także ważne jest utrzymywanie przyzwoitego poziomu i, w miarę indywidualnych
+            możliwości, ciągłe doskonalenie – tak żeby inni mieli z nas pożytek.</p>
+          <br>
+          <p>Docelowo chcemy oferować kadetom kompleksowe szkolenie zahaczające o możliwie wszystkie aspekty samolotu
+            F/A-18C. Oczywiście na tę chwilę, program jest dopiero rozwijany.</p>
+        </div>
+      </div>
+    </div>
     <UnderConstruction></UnderConstruction>
   </div>
 </template>
@@ -10,16 +28,64 @@ import UnderConstruction from "@/pages/home/components/UnderConstruction";
 
 export default {
   name: "SectionSquardon",
-  components: {UnderConstruction}
+  data() {
+    return {
+    };
+  },
+  components: { UnderConstruction }
 
 };
 </script>
 
 <style scoped lang="scss">
 
-h2 {
-  display: inline-block;
-  color: $wombat_white;
-  font-size: 3rem;
+
+.squadron_info {
+  width: 80%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  @media screen and (max-width: $phone) {
+    width: 100%;
+  }
+
+  .content_block {
+    width: 80%;
+    padding-left: 5%;
+    padding-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    @media screen and (max-width: $tablet) {
+      padding-right: 5%;
+      width: 100%;
+    }
+
+    .content_element {
+      color: $wombat_white;
+    }
+
+    .title {
+      .wombat_line {
+        height: 5px;
+        width: 150px;
+        background-color: $wombat_yellow;
+      }
+
+      .wrapper_title {
+        height: auto;
+        font-size: 3rem;
+      }
+    }
+
+    .description {
+      font-size: 1.15rem;
+      padding: 1rem 0;
+    }
+
+  }
+
+
 }
 </style>
